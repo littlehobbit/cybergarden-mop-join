b8cf272f9340a24083da46a184cd5479887e89ca
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-12-10 17:51:32 +0300

    test

---
+++ client/app-mobile/app/src/main/java/com/example/appmobile/SignUpScreen.java
@@ -0,0 +1,14 @@
+package com.example.appmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_screen);
+    }
+}
